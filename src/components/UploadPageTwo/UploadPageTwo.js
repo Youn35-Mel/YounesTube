c8ebe82e042891d950956b575111109c629fd314
Younes Mel <103428027+Youn35-Mel@users.noreply.github.com>
2022-11-02 19:24:00 +0000

    tiny mce upload

--- src/components/UploadPageTwo/UploadPageTwo.js
+++ src/components/UploadPageTwo/UploadPageTwo.js
@@ -8,6 +8,7 @@ import {
   uploadBytesResumable,
   listAll,
   list,
+  deleteObject,
 } from "firebase/storage";
 import { v4 } from "uuid";
 
@@ -15,7 +16,9 @@ import profile from "../../assets/Images/profile.jpg";
 import "./UploadPageTwo.scss";
 
 const UploadPageTwo = () => {
+  const [videoAsset, setVideoAsset] = useState(null);
   const [progress, setProgress] = useState(0);
+
   const formHandler = (e) => {
     e.preventDefault();
     const file = e.target[0].files[0];
@@ -40,10 +43,24 @@ const UploadPageTwo = () => {
       () => {
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
           console.log("File available at", downloadURL);
+          setVideoAsset(downloadURL);
         });
       }
     );
   };
+
+  const deleteImage = () => {
+    const deleteRef = ref(storage, videoAsset);
+    deleteObject(deleteRef)
+      .then(() => {
+        setVideoAsset(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // useEffect(() => {}, [videoAsset]);
 
   return (
     <section className="upload">
@@ -78,6 +95,12 @@ const UploadPageTwo = () => {
         </form>
         <hr />
         <h2>Uploading done {progress}%</h2>
+      </div>
+      <div>
+        <button onClick={deleteImage}>Delete</button>
+        <video
+          src={videoAsset}
+          style={{ width: "100%", height: "300px" }}></video>
       </div>
     </section>
   );
