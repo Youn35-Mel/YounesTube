4cbc8dc0786f5d4926b177dc96560b25d6b82d94
Younes Mel <103428027+Youn35-Mel@users.noreply.github.com>
2022-11-07 21:43:20 +0000

    changed the styling for chat

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,6 +1,7 @@
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase-config";
 import ChatMessage from "../ChatMessage/ChatMessage";
+import Message from "../Message/Message";
 import Login from "../../Pages/Login/Login";
 import React, { useState, useEffect, useRef } from "react";
 import { db } from "../../firebase-config";
@@ -12,24 +13,44 @@ import {
   Timestamp,
 } from "firebase/firestore";
 import { useCollectionData } from "react-firebase-hooks/firestore";
-import "./Chat.scss";
+// import "./Chat.scss";
+
+const style = {
+  main: `flex flex-col p-[10px]`,
+};
 const Chat = () => {
   const [user] = useAuthState(auth);
-  console.log(user);
+  // console.log(user);
+  // const scroll = useRef();
+  // const [messages, setMessages] = useState(null);
+
+  // useEffect(() => {
+  //   const articleRef = collection(db, "messages");
+  //   const q = query(articleRef, orderBy("createdAt", "desc"));
+  //   onSnapshot(q, (snapshot) => {
+  //     const messages = snapshot.docs.map((doc) => ({
+  //       id: doc.id,
+  //       ...doc.data(),
+  //     }));
+  //     setMessages(messages);
+  //     // console.log(messages);
+  //   });
+  // }, []);
+
+  ////
+  const [messages, setMessages] = useState([]);
   const scroll = useRef();
-  const [messages, setMessages] = useState(null);
 
   useEffect(() => {
-    const articleRef = collection(db, "messages");
-    const q = query(articleRef, orderBy("createdAt", "desc"));
-    onSnapshot(q, (snapshot) => {
-      const messages = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
+    const q = query(collection(db, "messages"), orderBy("timestamp"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let messages = [];
+      querySnapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
       setMessages(messages);
-      // console.log(messages);
     });
+    return () => unsubscribe();
   }, []);
 
   if (!messages) {
@@ -37,25 +58,37 @@ const Chat = () => {
   }
   return (
     <>
-      <div className="msgs">
+      <main className={style.main}>
         {messages &&
-          messages.map((message) => {
-            return (
-              <div
-                key={message.id}
-                className={`msg ${
-                  message.uid === user.uid ? "sent" : "received"
-                }`}>
-                <img className="msg-user" src={message.photoURL} alt="" />
-                <p>{message.text}</p>
-                <p>{message.userName}</p>
-              </div>
-            );
-          })}
-      </div>
+          messages.map((message) => (
+            <Message key={message.id} message={message} />
+          ))}
+      </main>
+      {/* Send Message Compoenent */}
       <ChatMessage scroll={scroll} />
-      <div ref={scroll}></div>
+      <span ref={scroll}></span>
     </>
+
+    // <>
+    //   <div className="msgs">
+    //     {messages &&
+    //       messages.map((message) => {
+    //         return (
+    //           <div
+    //             key={message.id}
+    //             className={`msg ${
+    //               message.uid === user.uid ? "sent" : "received"
+    //             }`}>
+    //             <img className="msg-user" src={message.photoURL} alt="" />
+    //             <p>{message.text}</p>
+    //             <p>{message.userName}</p>
+    //           </div>
+    //         );
+    //       })}
+    //   </div>
+    //   <ChatMessage scroll={scroll} />
+    //   <div ref={scroll}></div>
+    // </>
   );
 };
 
--- src/components/ChatMessage/ChatMessage.js
+++ src/components/ChatMessage/ChatMessage.js
@@ -2,58 +2,97 @@ import { db, auth } from "../../firebase-config";
 import React, { useState, useEffect, useRef } from "react";
 import { Timestamp, collection, addDoc } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
+const style = {
+  form: `h-14 w-full max-w-[728px]  flex text-xl absolute bottom-0`,
+  input: `w-full text-xl p-3 bg-gray-900 text-white outline-none border-none`,
+  button: `w-[20%] bg-green-500`,
+};
 
 const ChatMessage = ({ scroll }) => {
-  const [msg, setMsg] = useState("");
-  const [user] = useAuthState(auth);
-
-  console.log(user);
+  const [input, setInput] = useState("");
 
   const sendMessage = async (e) => {
     e.preventDefault();
-
+    if (input === "") {
+      alert("Please enter a valid message");
+      return;
+    }
+    const { uid, displayName } = auth.currentUser;
     await addDoc(collection(db, "messages"), {
-      text: msg,
-      photoURL: user.photoURL,
-      uid: user.uid,
-      createdAt: Timestamp.now().toDate(),
-      userName: user.displayName,
+      text: input,
+      name: displayName,
+      uid,
+      timestamp: Timestamp.now().toDate(),
     });
-    setMsg("");
+    setInput("");
     scroll.current.scrollIntoView({ behavior: "smooth" });
   };
+
   return (
-    <div>
-      <form onSubmit={sendMessage}>
-        <div className="sendMsg">
-          <input
-            style={{
-              width: "78%",
-              fontSize: "15px",
-              fontWeight: "550",
-              marginLeft: "5px",
-              marginBottom: "-3px",
-            }}
-            placeholder="Message..."
-            type="text"
-            value={msg}
-            onChange={(e) => setMsg(e.target.value)}
-          />
-          <button
-            style={{
-              width: "18%",
-              fontSize: "15px",
-              fontWeight: "550",
-              margin: "4px 5% -13px 5%",
-              maxWidth: "200px",
-            }}
-            type="submit">
-            Send
-          </button>
-        </div>
-      </form>
-    </div>
+    <form onSubmit={sendMessage} className={style.form}>
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        className={style.input}
+        type="text"
+        placeholder="Message"
+      />
+      <button className={style.button} type="submit">
+        Send
+      </button>
+    </form>
   );
 };
+//   const [msg, setMsg] = useState("");
+//   const [user] = useAuthState(auth);
+
+//   console.log(user);
+
+//   const sendMessage = async (e) => {
+//     e.preventDefault();
+
+//     await addDoc(collection(db, "messages"), {
+//       text: msg,
+//       photoURL: user.photoURL,
+//       uid: user.uid,
+//       createdAt: Timestamp.now().toDate(),
+//       userName: user.displayName,
+//     });
+//     setMsg("");
+//     scroll.current.scrollIntoView({ behavior: "smooth" });
+//   };
+//   return (
+//     <div>
+//       <form onSubmit={sendMessage}>
+//         <div className="sendMsg">
+//           <input
+//             style={{
+//               width: "78%",
+//               fontSize: "15px",
+//               fontWeight: "550",
+//               marginLeft: "5px",
+//               marginBottom: "-3px",
+//             }}
+//             placeholder="Message..."
+//             type="text"
+//             value={msg}
+//             onChange={(e) => setMsg(e.target.value)}
+//           />
+//           <button
+//             style={{
+//               width: "18%",
+//               fontSize: "15px",
+//               fontWeight: "550",
+//               margin: "4px 5% -13px 5%",
+//               maxWidth: "200px",
+//             }}
+//             type="submit">
+//             Send
+//           </button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// };
 
 export default ChatMessage;
